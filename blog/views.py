4b31d8fbdb7f2edddf21a05ac3e506a96b17233c
Albanek203 <oleglovchuk@ukr.net>
2022-11-11 09:56:51 +0200

    configure paths / render html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
